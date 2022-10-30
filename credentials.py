75f32a5a498d087832d9b464ede0fb5064e1ffc5
Anuvab Sen <Anuvab.Sen@studentambassadors.com>
2022-10-31 01:32:37 +0530

    first commit

---
+++ credentials.py
@@ -0,0 +1,6 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
+consumer_key = 'e1ugjFrUmbRNgj7zIRxevmEkE'
+consumer_secret = 'EdnsS7BcpGIqBZGKf5CbU9HBYLv49T2L76T5A0j5RVkVJYa4kv'  
+access_token = '1419239941704359940-6schaMF40uRv5oqZbqrFFvCzpt3b0V'
+access_token_secret = 'WBTaxYwNOMGh2DaFpJQjIW6Xlx0rxtfsvaPyP9vAXilmi'
